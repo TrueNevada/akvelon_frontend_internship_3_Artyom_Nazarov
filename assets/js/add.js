b3986d49f0e1a104d48df10fcc8e3991bc5f5f4a
TrueNevada <ricefieldsevilsamurai@gmail.com>
2021-05-08 12:26:48 +0300

    third commit

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,5 +1,12 @@
 //factorial function
-
 function factorial(n) {
   return n ? n * factorial(n - 1) : 1;
+}
+
+//reverse bits function
+function reverseBits (n) {
+  let binary = (n).toString(2);
+  let reverseBinary = binary.split("").reverse().join("");
+  let nonBinary = parseInt(reverseBinary, 2);
+  console.log(nonBinary);
 }
